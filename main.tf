de5da59b66a6ebb6069baad7e3e6103e8914c05d
Praful <praful.n007@gmail.com>
2024-01-14 12:21:12 +0000

    Errored the main.tf file for testing

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 ## Create EC2 instance 
 resource "aws_instance" "devops-server"{
   ami           = var.ami_id 
-  instance_type = var.instance_type 
+  instance_type = instance_type 
 
   tags = {
     Name = var.tag_name 
